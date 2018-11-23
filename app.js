e459bcf7d25f2d3fa76e85f723cf9ef06c7cccdd
diurivj <diurivj@icloud.com>
2018-11-23 17:15:04 -0600

    done putitas

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
+const session      = require("express-session");
+const MongoStore   = require("connect-mongo")(session);
 
 
 mongoose
@@ -29,6 +31,15 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(session({
+  secret: process.env.SECRET,
+  cookie: { maxAge: 60000 },
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60 // 1 day
+  })
+}));
 
 // Express View engine setup
 
--- routes/auth.js
+++ routes/auth.js
@@ -44,4 +44,37 @@ router.post('/signup', (req, res, next) => {
   })
 });
 
+router.get('/login', (req, res) => {
+  if (req.session.currentUser) return res.redirect('/profile');
+  res.render('auth/login');
+});
+
+router.post('/login', (req, res, next) => {
+  const { email, password } = req.body;
+  User.findOne({ email })
+  .then(user => {
+    if (user === null) {
+      return res.render('auth/login', {
+        message: 'This user doesnt exist, please sign up first'
+      })
+    }
+    if (bcrypt.compareSync(password, user.password)) {
+      req.session.currentUser = user;
+      res.redirect('/profile');
+    } else {
+      return res.send('Esto es sospechoso alv 🕵🏻‍♂️')
+    }
+  })
+  .catch(err => {
+    console.log(err);
+  })
+});
+
+router.get('/logout', (req, res) => {
+  req.session.destroy(err => {
+    if (err) res.send(err);
+    else return res.redirect('/auth/login');
+  })
+})
+
 module.exports = router;
